8f41a3ea82416fc3aa783bc4b2f0091d312d93eb
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2024-03-14 22:23:27 -0300

    added implement get view in root

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { IReview } from './components/getReviews'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  getReviews(): Promise<IReview[]> {
+    return this.appService.getReviews()
   }
 }
