b00fcb230486f2ddccff71cbf8553eff97e858c8
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2024-03-17 12:20:13 -0300

    added authenticate route

--- src/controllers/authenticate-controller.ts
+++ src/controllers/authenticate-controller.ts
@@ -1,16 +1,56 @@
-import { Controller, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { compare } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.services'
+import { z } from 'zod'
+
+const AuthenticateBodySchema = z.object({
+  email: z.string().email('Email inválido'),
+  password: z.string().min(6, 'A senha deve conter pelo menos 6 dígitos'),
+})
+
+type AuthenticateBodySchema = z.infer<typeof AuthenticateBodySchema>
 
 @Controller('/sessions')
 export class AuthenticateController {
-  constructor(private jwt: JwtService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
 
   @Post()
-  async handle() {
-    const token = this.jwt.sign({
-      sub: 'user-id',
+  @HttpCode(200)
+  @UsePipes(new ZodValidationPipe(AuthenticateBodySchema))
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async handle(@Body() body: AuthenticateBodySchema) {
+    const { email, password } = AuthenticateBodySchema.parse(body)
+
+    const user = await this.prisma.user.findUnique({
+      where: { email },
     })
 
-    return token
+    if (!user) {
+      throw new UnauthorizedException('Email ou senha inválido!')
+    }
+
+    const isPasswordValid = await compare(password, user.password)
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Email ou senha inválido!')
+    }
+
+    const accessToken = this.jwt.sign({ sub: user.id })
+
+    return {
+      access_token: accessToken,
+    }
   }
 }
