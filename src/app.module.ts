a6b2569c18f022d8d978934fab55051515a8427a
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2024-03-17 12:11:47 -0300

    Implement JWT security

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/authenticate-controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.services'
@@ -12,8 +14,9 @@ import { PrismaService } from './prisma/prisma.services'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [AppController, CreateAccountController],
+  controllers: [AppController, CreateAccountController, AuthenticateController],
   providers: [AppService, PrismaService],
 })
 export class AppModule {}
