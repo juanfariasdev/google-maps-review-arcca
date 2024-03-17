657a8d0fd23f96353c3b025593a03947869722bb
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2024-03-17 11:38:11 -0300

    added validate env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.services'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController, CreateAccountController],
   providers: [AppService, PrismaService],
 })
